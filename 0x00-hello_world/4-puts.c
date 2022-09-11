18282ef7c8b0b9a8b821d267beb06d77f4d297ef
JamesChima <chima.mjames@hotmail.com>
2022-09-11 02:38:33 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+
+ * main - print the string in the put function
+
+ *
+
+ * Description: using the main function
+
+ * this program will print out the following "Programming is like building a multilingual puzzle
+
+ * Return: 0
+
+ */
+
+int main(void)
+  
+{ puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+}
