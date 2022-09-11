1ed14a344fa1143196f7f7c33443bff8727b16e8
JamesChima <chima.mjames@hotmail.com>
2022-09-11 06:51:19 -0700

    6-size.c
    git commit -m 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - print string using the print function
+*
+* Description: using the main function
+* this program prints "the size of various types on the computer"
+* Return: 0
+*/
+int main(void)
+{
+char c;
+int i;
+long li;
+long long ll;
+float f;
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
+}
