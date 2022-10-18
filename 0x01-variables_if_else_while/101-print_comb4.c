61b3719e318f67edef9a467240ffd798f847b9c2
JamesChima <chima.mjames@hotmail.com>
2022-10-18 06:16:30 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
-
 /**
-* main - Function
-*
-* Return: Always 0.
-*/
+ * main - program that prints all possible different combinations of 2 digits
+ * Return: 0
+ */
 int main(void)
 {
-int d1, d2, d3
-for (d1 = 0; d1 < 8; d1++)
-{
-for (d2 = d1 + 1; d2 < 9; d2++)
-{
-for (d3 = d2 + 1; d3 < 10; d3++)
-{
-putchar((d1 % 10) + '0');
-putchar((d2 % 10) + '0');
-putchar((d3 % 10) + '0');
-if (d1 == 7 && d2 == 8 && d3 == 9)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+  int c = 0;
+  int f_d;
+  int l_d;
+
+  while (c <= 99)
+    {
+      f_d = (c / 10 + '0');
+      l_d = (c % 10 + '0');
+
+      if (f_d < l_d)
+	{
+	  putchar(f_d);
+	  putchar(l_d);
+
+	  if (c != 89)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+      c++;
+    }
+  putchar('\n');
+  return (0);
 }
